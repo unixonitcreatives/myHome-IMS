c994ca37c32b92920618db3ab24ea0c86e67e192
vincecachi <vincentcachila@gmail.com>
2019-05-21 15:40:03 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'remotemysql.com');
-define('DB_USERNAME', 'nCJRDnImYY');
-define('DB_PASSWORD', 'CFZLAtjF1n');
-define('DB_NAME', 'nCJRDnImYY');
+define('DB_USERNAME', 'xAcmiyzWOF');
+define('DB_PASSWORD', 'cQZ2tArDGK');
+define('DB_NAME', 'xAcmiyzWOF');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die("POTA" . mysqli_connect_error());
@@ -14,3 +14,5 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
+
+Connected Succesful!
