c2ded5e39fb6441cbc3547b715902c39ebffcdd2
vincecachi <vincentcachila@gmail.com>
2019-05-21 14:51:47 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,12 +2,12 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', '69.195.124.192');
-define('DB_USERNAME', 'longrich_myhome');
-define('DB_PASSWORD', 'myhome');
-define('DB_NAME', 'longrich_myhome_db');
+define('DB_USERNAME', 'longrich_adaptx');
+define('DB_PASSWORD', 'Joff123!!!');
+define('DB_NAME', 'longrich_adapt');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die("POTA");
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die("POTA" . mysqli_connect_error());
  
 // Check connection
 if($link === false){
