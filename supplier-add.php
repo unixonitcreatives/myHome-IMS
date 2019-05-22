b4e686858843b5b735168dcede7ff08c41e206f2
vincecachi <vincentcachila@gmail.com>
2019-05-22 11:44:45 +0800

    Update supplier-add.php

--- supplier-add.php
+++ supplier-add.php
@@ -110,9 +110,56 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </section>
 
     <!-- Main content -->
+    <section class="content">
+    <div class="col-md-6">
+          <!-- general form elements -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Supplier's Information</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form">
+              <div class="box-body">
+                <div class="form-group">
+                  <label>Suppliers</label>
+                  <input type="text" class="form-control" placeholder="Suppliers">
+                </div>
 
+                <div class="form-group">
+                  <label>Contact Person</label>
+                  <input type="text" class="form-control" placeholder="Contact Person">
+                </div>
+
+                <div class="form-group">
+                  <label>Phone</label>
+                  <input type="text" class="form-control" placeholder="Phone">
+                </div>
+
+                <div class="form-group">
+                  <label>Email</label>
+                  <input type="text" class="form-control" placeholder="Email">
+                </div>
+
+                <div class="form-group">
+                  <label>Address</label>
+                  <input type="text" class="form-control" placeholder="Address">
+                </div>
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Save</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
+
+
+        </div>
     <!-- /.content -->
   </div>
+</section>
   <!-- /.content-wrapper -->
 
   <footer class="main-footer">
