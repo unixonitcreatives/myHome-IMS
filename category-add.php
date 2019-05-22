fae85017e7d9c196ea03c65d66195f1965518782
vincecachi <vincentcachila@gmail.com>
2019-05-22 13:17:30 +0800

    Update category-add.php

--- category-add.php
+++ category-add.php
@@ -110,7 +110,35 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </section>
 
     <!-- Main content -->
+<section class="content">
+    <div class="col-md-6">
+          <!-- general form elements -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Product Category</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form">
+              <div class="box-body">
+                <div class="form-group">
+                  <label>Category Name</label>
+                  <input type="text" class="form-control" placeholder="Category e.g: Food, Clothes, Appliances">
+                </div>
+              <!-- /.box-body -->
 
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Save</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
+
+
+        </div>
+    <!-- /.content -->
+  </div>
+</section>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
