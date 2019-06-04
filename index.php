6eac1c6e74f09d0b4ac94620d85806e4b6c09f1e
vincecachi <vincentcachila@gmail.com>
2019-06-04 14:54:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         <!-- AdminLTE Skins. Choose a skin from the css/skins
 folder instead of downloading all of them to reduce the load. -->
         <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-        <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -101,7 +100,7 @@ folder instead of downloading all of them to reduce the load. -->
                             <p><?php echo htmlspecialchars($_SESSION["username"]); ?></p>
                             <!-- Status -->
                             <a href="#"><i class="fa fa-circle text-success"></i>
-
+                                
                                 <?php
                                     switch (connection_status())
                                     {
@@ -228,6 +227,8 @@ folder instead of downloading all of them to reduce the load. -->
 
                         <li><a href="support.php"><i class="fa fa-superpowers"></i> <span>Support</span></a>
                         </li>
+                        <li><a href="logout.php"><i class="fa fa-close"></i> <span>Logout</span></a>
+                        </li>
                     </ul>
                 </section>
                 <!-- /.sidebar -->
@@ -247,7 +248,91 @@ folder instead of downloading all of them to reduce the load. -->
                         <li><a href="index.php"><i class="fa fa-dashboard active"></i> Dashboard</a></li>
                     </ol>
                 </section>
+                    <section class="content">
 
+
+
+
+
+      <!-- =========================================================== -->
+
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>150</h3>
+
+              <p>New Orders</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-shopping-cart"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+              <p>Bounce Rate</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>44</h3>
+
+              <p>User Registrations</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>65</h3>
+
+              <p>Unique Visitors</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+      <!-- /.row -->
+
+      <!-- =========================================================== -->
+        
+
+    </section>
                 <!-- Main content -->
 
                 <!-- /.content -->
@@ -276,18 +361,7 @@ immediately after the control sidebar -->
         <script src="dist/js/adminlte.min.js"></script>
         <!-- AdminLTE for demo purposes -->
         <script src="dist/js/demo.js"></script>
-        <!-- Alert animation -->
-        <script type="text/javascript">
-        $(document).ready(function () {
 
-          window.setTimeout(function() {
-            $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
-              $(this).remove();
-            });
-          }, 1000);
-
-        });
-        </script>
 
 
     </body>
